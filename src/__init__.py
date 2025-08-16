e1eae7183919eeb927584c505fb279bd8d92bda5
ddogok <ddogok1210@gmail.com>
2025-08-17 00:39:40 +0900

    frontend

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Code Weaver - Static code analysis with dead code detection and 3D visualization."""
-
-__version__ = "1.0.0"
-__author__ = "Code Weaver Team"
--- src/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Controllers for Code Weaver API."""
-
-from .analysis_controller import AnalysisController
-
-__all__ = ["AnalysisController"]
